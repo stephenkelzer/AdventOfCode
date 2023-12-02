751813ffb4606d0bcdee587525a902774b2f9ba7
Stephen Kelzer <stephen@jump.com>
2023-12-01 22:21:32 -0600

    adding day_2 placeholder

---
+++ days/day_2/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world");
+}
+
+#[cfg(test)]
+mod day_2 {
+    // use super::*;
+
+    #[test]
+    fn returns_expected_value() {
+        assert_eq!(1, 1);
+    }
+}
