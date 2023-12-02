88bd058d21b131d35a056607fd9e1e6e8b67d3af
Stephen Kelzer <stephen@jump.com>
2023-12-02 08:18:48 -0600

    day 3 placeholder

---
+++ days/day_3/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    println!("part_one: {}", run_part_one());
+    println!("part_two: {}", run_part_two());
+}
+
+fn run_part_one() -> i32 {
+    123
+}
+
+fn run_part_two() -> i32 {
+    123
+}
+
+#[cfg(test)]
+mod day_1 {
+    use super::*;
+
+    #[test]
+    fn part_one() {
+        assert_eq!(run_part_one(), 123);
+    }
+
+    #[test]
+    fn part_two() {
+        assert_eq!(run_part_two(), 123);
+    }
+}
