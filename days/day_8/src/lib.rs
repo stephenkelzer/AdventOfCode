44ce7e0ee562f52f123dd304db2b99cb0b8739bd
Stephen Kelzer <stephen@jump.com>
2023-12-07 23:34:22 -0600

    day 8, part 1

--- days/day_8/src/lib.rs
+++ days/day_8/src/lib.rs
@@ -4,10 +4,47 @@ mod day_8 {
 
     #[test]
     fn part_one() {
-        // TODO
-        // let lines = include_str!("input.txt").lines();
+        let mut lines = include_str!("input.txt").lines();
+        let instructions = lines.next().unwrap().chars().collect_vec();
+        let instructions_length = instructions.len();
 
-        assert_eq!(123, 123);
+        // Vec<(curr_location, left_option, right_option)>
+        let nodes = lines
+            .skip(1)
+            .map(|l| {
+                let (location, options) = l.split_once(" = ").unwrap();
+                let (l, r) = options.split_once(", ").unwrap();
+                (location.to_string(), l.replace('(', ""), r.replace(')', ""))
+            })
+            .collect_vec();
+
+        let mut steps_taken: usize = 0;
+        let mut current_node = nodes
+            .iter()
+            .find(|(location, _, _)| location == "AAA")
+            .unwrap();
+        let mut made_it = false;
+
+        while !made_it {
+            let current_instruction = instructions[steps_taken % instructions_length];
+
+            let next_node = nodes
+                .iter()
+                .find(|(location, _, _)| {
+                    (current_instruction == 'L' && current_node.1.eq(location))
+                        || (current_instruction == 'R' && current_node.2.eq(location))
+                })
+                .unwrap();
+
+            current_node = next_node;
+            steps_taken += 1;
+
+            if next_node.0.eq("ZZZ") {
+                made_it = true
+            }
+        }
+
+        assert_eq!(steps_taken, 16043);
     }
 
     #[test]
