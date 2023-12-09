6f3877ae6e944739fcbb517b0b6a54d4db9e749a
Stephen Kelzer <stephen@jump.com>
2023-12-09 00:10:12 -0600

    day 9, part 2

--- days/day_9/src/lib.rs
+++ days/day_9/src/lib.rs
@@ -42,8 +42,39 @@ mod day_9 {
 
     #[test]
     fn part_two() {
-        // let mut lines = include_str!("input.txt").lines();
+        let lines = include_str!("input.txt").lines();
+        let list = lines
+            .map(|l| {
+                l.split_whitespace()
+                    .map(|x| x.parse::<isize>().unwrap())
+                    .collect_vec()
+            })
+            .collect_vec();
 
-        assert_eq!(123, 123);
+        let dive = |input: Vec<isize>| -> Vec<isize> {
+            input
+                .windows(2)
+                .filter_map(|w| Some(w[1] - w[0]))
+                .collect_vec()
+        };
+
+        let mut results = vec![];
+
+        for row in list {
+            let mut layers: Vec<Vec<isize>> = vec![row];
+            while !layers.last().unwrap().iter().all(|x| x == &0) {
+                layers.push(dive(layers.last().unwrap().to_vec()));
+            }
+
+            let mut x_factor = layers.iter().rev().skip(1).take(1).collect_vec()[0][0];
+
+            for layer in layers.iter().rev().skip(2) {
+                x_factor = layer[0] - x_factor;
+            }
+
+            results.push(x_factor);
+        }
+
+        assert_eq!(results.iter().sum::<isize>(), 957);
     }
 }
