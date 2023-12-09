03f0ebe51dad5b4de4846c4a337b019125bdfe03
Stephen Kelzer <stephen@jump.com>
2023-12-08 23:31:47 -0600

    day 9, part 1

--- days/day_9/src/lib.rs
+++ days/day_9/src/lib.rs
@@ -1,11 +1,43 @@
 #[cfg(test)]
 mod day_9 {
+    use itertools::Itertools;
 
     #[test]
     fn part_one() {
-        // let mut lines = include_str!("input.txt").lines();
+        let lines = include_str!("input.txt").lines();
+        let list = lines
+            .map(|l| {
+                l.split_whitespace()
+                    .map(|x| x.parse::<isize>().unwrap())
+                    .collect_vec()
+            })
+            .collect_vec();
 
-        assert_eq!(123, 123);
+        let dive = |input: Vec<isize>| -> Vec<isize> {
+            input
+                .windows(2)
+                .filter_map(|w| Some(w[1] - w[0]))
+                .collect_vec()
+        };
+
+        let mut results = vec![];
+
+        for row in list {
+            let mut layers: Vec<Vec<isize>> = vec![row];
+            while !layers.last().unwrap().iter().all(|x| x == &0) {
+                layers.push(dive(layers.last().unwrap().to_vec()));
+            }
+
+            results.push(
+                layers
+                    .iter()
+                    .rev()
+                    .map(|x| x.last().unwrap())
+                    .sum::<isize>(),
+            )
+        }
+
+        assert_eq!(results.iter().sum::<isize>(), 1953784198);
     }
 
     #[test]
