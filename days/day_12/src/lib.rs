fafbb2e93ae4bb056e917ecf8fff5eb452fd2d4d
Stephen Kelzer <stephen@jump.com>
2023-12-11 01:32:27 -0600

    day 12 placeholder

---
+++ days/day_12/src/lib.rs
@@ -0,0 +1,13 @@
+#[cfg(test)]
+mod day_12 {
+    #[test]
+    fn part_01() {
+        //let lines = include_str!("input.txt").lines()
+        assert_eq!(123, 123);
+    }
+
+    #[test]
+    fn part_02() {
+        assert_eq!(123, 123);
+    }
+}
