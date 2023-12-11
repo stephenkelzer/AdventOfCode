7c18360d278645468d8f1bb09cc87cdb43d3faf9
Stephen Kelzer <stephen@jump.com>
2023-12-10 22:14:11 -0600

    cleanup

--- days/day_10/src/lib.rs
+++ days/day_10/src/lib.rs
@@ -49,8 +49,6 @@ mod day_10 {
         let grid_w = grid[0].len();
         let grid_h = grid.len();
 
-        let is_inbounds = |Point(r, c)| r <= grid_h && c <= grid_w;
-
         let starting_spots = NEIGHBORS_MAP
             .iter()
             .filter_map(|[r_mod, c_mod]| {
@@ -62,12 +60,12 @@ mod day_10 {
                 let modded_r = modded_r as usize;
                 let modded_c = modded_c as usize;
 
-                let x = Point(modded_r, modded_c);
+                let point = Point(modded_r, modded_c);
 
-                if is_inbounds(x.clone()) && grid[modded_r][modded_c] != '.' {
+                if point.0 <= grid_h && point.1 <= grid_w && grid[modded_r][modded_c] != '.' {
                     let char = grid[modded_r][modded_c];
                     if char != '.' {
-                        return Some((x, char));
+                        return Some((point, char));
                     }
                 }
 
@@ -109,26 +107,34 @@ mod day_10 {
             neighbors
         };
 
-        let mut spot_1_history = vec![position, starting_spots[0]];
-        let mut spot_2_history = vec![position, starting_spots[1]];
+        let mut vertices: VecDeque<Point> =
+            VecDeque::from(vec![starting_spots[0], position, starting_spots[1]]);
 
-        while spot_1_history.last().unwrap() != spot_2_history.last().unwrap() {
-            spot_1_history.push(
-                *get_neighbors(*spot_1_history.last().unwrap())
-                    .iter()
-                    .find(|x| !spot_1_history.contains(x))
-                    .unwrap(),
-            );
+        let mut found_end = false;
 
-            spot_2_history.push(
-                *get_neighbors(*spot_2_history.last().unwrap())
-                    .iter()
-                    .find(|x| !spot_2_history.contains(x))
-                    .unwrap(),
-            );
+        while !found_end {
+            if let Some(point) = get_neighbors(*vertices.front().unwrap())
+                .iter()
+                .find(|x| !vertices.contains(x))
+            {
+                vertices.push_front(*point);
+            } else {
+                found_end = true;
+            }
+
+            if let Some(point) = get_neighbors(*vertices.back().unwrap())
+                .iter()
+                .find(|x| !vertices.contains(x))
+            {
+                vertices.push_back(*point);
+            } else {
+                found_end = true;
+            }
         }
 
-        assert_eq!(spot_1_history.len() - 1, 6846);
+        let answer = vertices.len() / 2;
+
+        assert_eq!(answer, 6846);
     }
 
     #[test]
@@ -163,8 +169,6 @@ mod day_10 {
         let grid_w = grid[0].len();
         let grid_h = grid.len();
 
-        let is_inbounds = |Point(r, c)| r <= grid_h && c <= grid_w;
-
         let starting_spots = NEIGHBORS_MAP
             .iter()
             .filter_map(|[r_mod, c_mod]| {
@@ -176,12 +180,12 @@ mod day_10 {
                 let modded_r = modded_r as usize;
                 let modded_c = modded_c as usize;
 
-                let x = Point(modded_r, modded_c);
+                let point = Point(modded_r, modded_c);
 
-                if is_inbounds(x.clone()) && grid[modded_r][modded_c] != '.' {
+                if point.0 <= grid_h && point.1 <= grid_w && grid[modded_r][modded_c] != '.' {
                     let char = grid[modded_r][modded_c];
                     if char != '.' {
-                        return Some((x, char));
+                        return Some((point, char));
                     }
                 }
 
@@ -249,12 +253,11 @@ mod day_10 {
         }
 
         let loop_length = vertices.len();
-        let mut area = 0.0;
 
-        for i in 0..loop_length {
+        let area = (0..loop_length).fold(0.0, |acc, i| {
             let j = (i + 1) % loop_length;
-            area += (vertices[i].0 * vertices[j].1) as f64 - (vertices[j].0 * vertices[i].1) as f64;
-        }
+            acc + (vertices[i].0 * vertices[j].1) as f64 - (vertices[j].0 * vertices[i].1) as f64
+        });
 
         let answer = (area.abs() / 2.0) + 1. - loop_length as f64 / 2.;
 
