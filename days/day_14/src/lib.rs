a281c8d8121101c328c5d84cf9730ea82521e150
Stephen Kelzer <stephen@jump.com>
2023-12-13 22:58:19 -0600

    day 14 placeholder

---
+++ days/day_14/src/lib.rs
@@ -0,0 +1,18 @@
+#[cfg(test)]
+mod day_14 {
+    use itertools::Itertools;
+
+    #[test]
+    fn part_01() {
+        let grids = include_str!("input.txt");
+
+        assert_eq!(123, 123);
+    }
+
+    #[test]
+    fn part_02() {
+        let grids = include_str!("input.txt");
+
+        assert_eq!(123, 123);
+    }
+}
