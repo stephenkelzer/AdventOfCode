b0bae0d8e0a4817c9013482bfcf8eba5c2d0717c
Stephen Kelzer <stephen@jump.com>
2023-12-14 00:31:38 -0600

    day 14, part 1 checking

--- days/day_14/src/lib.rs
+++ days/day_14/src/lib.rs
@@ -4,9 +4,59 @@ mod day_14 {
 
     #[test]
     fn part_01() {
-        let grids = include_str!("input.txt");
+        let mut grid = include_str!("input.txt")
+            .lines()
+            .map(|l| l.chars().collect_vec())
+            .collect_vec();
 
-        assert_eq!(123, 123);
+        for r in 1..grid.len() {
+            for c in 0..grid[r].len() {
+                let col = grid[r][c];
+                println!("{:?}|{:?} => {:?}", r, c, col);
+                if col != 'O' {
+                    continue;
+                }
+
+                let mut high_r: Option<usize> = None;
+                let mut curr_r = r.clone() - 1;
+
+                loop {
+                    println!("checking: {}|{}", curr_r, c);
+
+                    if grid[curr_r][c] != '.' {
+                        break;
+                    }
+
+                    high_r = Some(curr_r);
+
+                    if curr_r == 0 {
+                        break;
+                    }
+
+                    curr_r -= 1;
+                }
+
+                if let Some(high_r) = high_r {
+                    // slide the rock up
+                    println!("MOVING: {}", high_r);
+                    grid[high_r][c] = 'O';
+                    grid[r][c] = '.';
+                }
+            }
+        }
+
+        let grid_height = grid.len();
+
+        let answer = grid
+            .iter()
+            .enumerate()
+            .map(|(r, row)| {
+                let rock_count = row.iter().filter(|x| **x == 'O').count();
+                (grid_height - r) * rock_count
+            })
+            .sum::<usize>();
+
+        assert_eq!(answer, 106990);
     }
 
     #[test]
