c92987c221c4c97d26214568404f009fc2d5ad3b
Stephen Kelzer <stephen@jump.com>
2023-12-26 23:12:32 -0600

    updates to scaffold

--- cli/src/commands/scaffold.rs
+++ cli/src/commands/scaffold.rs
@@ -1,10 +1,5 @@
-use std::{
-    fs::OpenOptions,
-    io::{Error, Write},
-    path::Path,
-};
-
 use core::Puzzle;
+use std::{fs::OpenOptions, io::Write, path::Path};
 
 const MAIN_FILE_TEMPLATE: &str = r#"
 aoc_solver_derive::aoc_solver!(%YEAR%, %DAY%, part1, part2);
@@ -35,49 +30,36 @@ core.intertools = true
 aoc-solver-derive.workspace = true
 "#;
 
-fn safe_create_file(puzzle: &Puzzle, name: &str, content: Option<&str>) -> () {
+fn create_file(puzzle: &Puzzle, name: &str, content: Option<&str>) -> () {
     let path = Path::new(&puzzle.get_crate_root()).join(name);
-    // let path = format!("src/puzzles/{}/{}/{}.rs", puzzle.year, puzzle.day, name);
 
-    let try_run = || -> Result<(), Error> {
-        let file_path = Path::new(&path);
-
-        if let Some(prefix) = file_path.parent() {
-            std::fs::create_dir_all(prefix)?;
-        }
-
-        let mut file = OpenOptions::new()
-            .write(true)
-            .create_new(true)
-            .open(file_path)?;
-
-        if let Some(content) = content {
-            let contents = content
-                .replace("%YEAR%", &format!("{:04}", &puzzle.get_year()))
-                .replace("%DAY%", &format!("{:02}", puzzle.get_day()));
-
-            file.write_all(&contents.as_bytes())?
-        }
-
-        Ok(())
-    };
-
-    if let Err(e) = try_run() {
-        eprintln!("Failed to create file: {path:?}");
-        panic!("{e}");
-    } else {
-        println!("Wrote file: {path:?}");
+    if let Some(prefix) = path.parent() {
+        std::fs::create_dir_all(prefix).expect("Could not create directory.");
     }
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(&path)
+        .expect("Could not create file");
+
+    if let Some(content) = content {
+        let contents = content
+            .replace("%YEAR%", &format!("{:04}", &puzzle.get_year()))
+            .replace("%DAY%", &format!("{:02}", puzzle.get_day()));
+
+        file.write_all(&contents.as_bytes())
+            .expect("Could not write contents to file")
+    }
+
+    println!("Created file: {:?}", path);
 }
 
 pub fn handle(puzzle: Puzzle) {
-    let create_file_placeholder =
-        |name: &str, content: Option<&str>| -> () { safe_create_file(&puzzle, name, content) };
-
-    create_file_placeholder("src/main.rs", Some(MAIN_FILE_TEMPLATE));
-    create_file_placeholder("Cargo.toml", Some(TOML_FILE_TEMPLATE));
-    // create_file_placeholder("input.txt", None);
-    // create_file_placeholder("example.txt", None);
+    create_file(&puzzle, "src/main.rs", Some(MAIN_FILE_TEMPLATE));
+    create_file(&puzzle, "Cargo.toml", Some(TOML_FILE_TEMPLATE));
+    // create_file(&puzzle, "input.txt", None);
+    // create_file(&puzzle, "example.txt", None);
 
     println!("---");
     println!(
