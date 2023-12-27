519e7f987272d876ca7ae1e81df882bd399d4f66
Stephen Kelzer <stephen@jump.com>
2023-12-27 14:04:37 -0600

    properly validating latest_year in config

--- core/src/config.rs
+++ core/src/config.rs
@@ -1,5 +1,9 @@
+use chrono::Datelike;
 use dotenvy::dotenv;
 use std::env;
+
+const SESSION_TOKEN_KEY: &str = "AOC_SESSION_TOKEN";
+const DEFAULT_YEAR_KEY: &str = "AOC_DEFAULT_YEAR_OVERRIDE";
 
 #[derive(Debug, Clone)]
 pub struct Configuration {
@@ -12,22 +16,32 @@ impl Configuration {
     pub fn new() -> Self {
         dotenv().ok();
 
-        let latest_year = 2023;
-        let default_year = env::var("AOC_DEFAULT_YEAR")
-            .expect("Could not find `AOC_DEFAULT_YEAR` environment variable in `.env` file.")
-            .parse::<u16>()
-            .expect(
-                "`AOC_DEFAULT_YEAR` wasn't formatted in the expected format of u16 (e.g. '2023').",
-            );
+        let now = chrono::offset::Utc::now();
+        let curr_year: u16 = now
+            .year()
+            .try_into()
+            .expect("Couldn't parse curr_year into u16");
+        let latest_year: u16 = match now.month() {
+            12 => curr_year,
+            _ => curr_year - 1,
+        };
+
+        let default_year: u16 = match env::var(DEFAULT_YEAR_KEY) {
+            Ok(val) => val.parse::<u16>().expect(&format!(
+                "`{DEFAULT_YEAR_KEY}` wasn't formatted in the expected format of u16 (e.g. '{latest_year}')."
+            )),
+            Err(_) => latest_year,
+        };
 
         let allowed_year_range = 2015..=latest_year;
         if !allowed_year_range.contains(&default_year) {
-            panic!("`{default_year}` is out of the allowed range: {allowed_year_range:?}. Check your `.env` file and ensure that the `AOC_DEFAULT_YEAR` value is within the allowed range.");
+            panic!("`{default_year}` is out of the allowed range: {allowed_year_range:?}. Check your `.env` file and ensure that the `{DEFAULT_YEAR_KEY}` value is within the allowed range.");
         }
 
         Self {
-            session_token: env::var("AOC_SESSION_TOKEN")
-                .expect("Could not find `AOC_SESSION_TOKEN` environment variable in `.env` file."),
+            session_token: env::var(SESSION_TOKEN_KEY).expect(&format!(
+                "Could not find `{SESSION_TOKEN_KEY}` environment variable in `.env` file."
+            )),
             default_year,
             latest_year,
         }
